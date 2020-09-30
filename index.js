72a44f26651999c1e92bb9a79869f373548c797b
James Min <jmin499@gmail.com>
2020-09-30 18:23:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const currentLine = (newLine) => {
   }
 
 
-  let result = 'The line is currently:'
+  let result = 'The line is currently: '
 
   for(let i = 0; i < newLine.length; i++) {
-    result += ` ${i + 1}. ${newLine[i]}${i === newLine.length - 1 ? '' : ', '}`
+    result += `${i + 1}. ${newLine[i]}${i === newLine.length - 1 ? '' : ', '}`
   }
 
   return result;
